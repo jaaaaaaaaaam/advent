6f8a72ab0f5d5921130cd09cd230640d9d227d89
James <j.wallen.jones@googlemail.com>
2015-12-02 10:40:17 +0000

    Complete Day 2.

---
+++ day2/day2.php
@@ -0,0 +1,19 @@
+<?php
+
+$file = file('i');
+
+$paper = 0;
+$ribbon = 0;
+
+foreach ($file as $dimensions) {
+	$box = explode('x', $dimensions);
+	sort($box, 1);
+	$extra = $box[0]*$box[1];
+	$sides = $extra*2;
+	$tb = ($box[1]*$box[2])*2;
+	$fb = ($box[0]*$box[2])*2;
+	$ribbon += ($box[0]*$box[1]*$box[2]) + $box[0]*2 + $box[1]*2;
+	$paper += $extra+$sides+$tb+$fb;
+}
+
+echo "$paper $ribbon";
