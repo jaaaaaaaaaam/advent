76f57a072cf917398fefb5548ef3494babddcc0e
James <j.wallen.jones@googlemail.com>
2015-12-04 11:02:16 +0000

    down to 99 bytes

--- day4/day4-golfed.php
+++ day4/day4-golfed.php
@@ -1,1 +1,1 @@
-<?php $i='bgvyzdsv';$n=0;$x=4;while(++$x<7){while(strpos(md5($i.$n),str_repeat(0,$x))!==0){++$n;}echo"$n ";}
+<?php $n=0;$x=4;while(++$x<7){while(strpos(md5("bgvyzdsv$n"),str_repeat(0,$x))!==0)++$n;echo"$n ";}
